c068c4c22d079420478375f95f757daff95220c7
Ahammad204 <ahammadcode@gmail.com>
2025-04-11 23:08:49 +0600

    create security section/homepage

--- investmate/src/Pages/Home/Home/Home.jsx
+++ investmate/src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Banner } from '../Banner/Banner';
 import { Opportunities } from '../Opportunities/Opportunities';
 import { Return } from '../Return/Return';
+import { Secure } from '../Secure/Secure';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Opportunities></Opportunities>
             <Return></Return>
+            <Secure></Secure>
         </div>
     );
 };
