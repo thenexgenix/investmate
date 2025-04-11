fe69084ea7d92efac4f743d9e7c437496a04a4e7
Ahammad204 <ahammadcode@gmail.com>
2025-04-11 21:35:56 +0600

    create Opportunities section

--- investmate/src/Pages/Home/Home/Home.jsx
+++ investmate/src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Banner } from '../Banner/Banner';
+import { Opportunities } from '../Opportunities/Opportunities';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Opportunities></Opportunities>
         </div>
     );
 };
---
+++ investmate/src/Pages/Home/Opportunities/Opportunities.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import oppur1 from "../../../assets/oppu1.png"
+import oppur2 from "../../../assets/oppu2.png"
+import oppur3 from "../../../assets/oppu3.png"
+
+export const Opportunities = () => {
+  return (
+    <>
+        <div className='flex justify-around gap-5 my-10'>
+            <div className='flex gap-6'>
+                <img src={oppur1} alt="" />
+                <div>
+                    <h1 className='font-bold text-xl'>Investment Transparency</h1>
+                    <p>Full disclosure on all investments, ensuring you know exactly where your money is going.</p>
+                </div>
+            </div>
+            <div className='flex gap-6'>
+                <img src={oppur2} alt="" />
+                <div>
+                    <h1 className='font-bold text-xl'>Islamic Finance Principles</h1>
+                    <p>All investments are vetted to comply with Shariah law and ethical standards.</p>
+                </div>
+            </div>
+            <div className='flex gap-6'>
+                <img src={oppur3} alt="" />
+                <div>
+                    <h1 className='font-bold text-xl'>Diverse Opportunities</h1>
+                    <p>Access a wide range of Halal investment options across various sectors and risk levels.</p>
+                </div>
+            </div>
+          
+        </div>
+    </>
+  )
+}
