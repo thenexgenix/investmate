860472d8fed42bb6ac05c309b2cdc7e2d0337a38
Ahammad204 <ahammadcode@gmail.com>
2025-04-12 14:31:38 +0600

    create Responsive connect section/projectPage

--- investmate/src/Pages/Project/Project/Project.jsx
+++ investmate/src/Pages/Project/Project/Project.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ProjectBannerPage } from '../ProjectBannerPage/ProjectBannerPage';
+import { ConnectSection } from '../ConnectSection/ConnectSection';
 
 const Project = () => {
     return (
         <div className='px-3'>
             <ProjectBannerPage></ProjectBannerPage>
+            <ConnectSection></ConnectSection>
         </div>
     );
 };
